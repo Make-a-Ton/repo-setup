b82bf40e7565c0d89fb361493a676fe9808ba673
Afeef <mohammedafeef@afeef-2.local>
2022-10-27 15:39:06 +0530

    wip: Add user routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,7 +5,17 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="home" element={<div>home</div>} />
+        <Route path="users">
+          <Route path="/doctors">
+            <Route path="search" element={<div>search</div>}/>
+            <Route path=":id/tokens" >
+              <Route path="list" element={<div>list</div>}/>
+              <Route path="confirm" element={<div>confirm</div>}/>
+            </Route>
+          </Route>
+          <Route path="login" element={<div>login</div>}/>
+          <Route path="signup" element={<div>signup</div>}/>
+        </Route>
       </Routes>
     </Router>
   );
